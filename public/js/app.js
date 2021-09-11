bca2952dff724c3ece83e690b7b9f76ef072efa2
saloni <salonisinghal997@gmail.com>
2021-09-11 10:48:03 +0530

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+console.log('client-side js is loaded')
+
+const weatherForm=document.querySelector('form');
+const search=document.querySelector('input');
+
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const location=search.value;
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    response.json().then((data)=>{
+        console.log(data);
+        if(data.error)
+        console.log(data.error)
+        else{
+            console.log(data.location);
+            console.log(data.forecast);
+        }
+    })
+})
+
+})
